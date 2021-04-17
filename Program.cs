11ba749acefe4908821d38667362b34e9e9e52cf
ezralight@outlook.com.vn <vuphamhocvienmatma@gmail.com>
2021-04-17 21:18:39 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lession1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;         
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Ánh óc chó, ánh ăn cứt gà, ánh cười khà khà");
+            //}
+            //for (int i = 10 - 1; i >= 0; i--)
+            //{
+
+            //}
+
+            //List<String> a = new List<string>();
+            //a.Add("aaaa");
+            //a.Add("bbbb");
+            //a.Add("ccccc");
+
+            //foreach (var item in a)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("biến J là: " + j);
+                }
+                Console.WriteLine("biến I là: "  + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
