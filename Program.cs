90d4f8f37445af7a1429a867a5c9b474537c0757
ezralight@outlook.com.vn <vuphamhocvienmatma@gmail.com>
2021-04-24 20:15:51 +0700

    ánh óc chó

--- Program.cs
+++ Program.cs
@@ -8,33 +8,49 @@ namespace Lession1
     {
         static void Main(string[] args)
         {
-            //Console.OutputEncoding = Encoding.UTF8;         
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine("Ánh óc chó, ánh ăn cứt gà, ánh cười khà khà");
-            //}
-            //for (int i = 10 - 1; i >= 0; i--)
-            //{
+            Console.OutputEncoding = Encoding.UTF8;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ánh óc chó, ánh ăn cứt gà, ánh cười khà khà");
+            }
+            for (int i = 10 - 1; i >= 0; i--)
+            {
 
-            //}
+            }
 
-            //List<String> a = new List<string>();
-            //a.Add("aaaa");
-            //a.Add("bbbb");
-            //a.Add("ccccc");
+
 
             //foreach (var item in a)
             //{
             //    Console.WriteLine(item);
             //}
 
-            for (int i = 0; i < 10; i++)
+            int[] Kteam = new int[3];
+
+            string[] Kteam1 = new string[] { "Anh", "Love", "Em" };
+
+            Kteam1.SetValue("Tao", 0);
+            Kteam1.SetValue("May", 2);
+            Kteam1[0] = "";
+
+            object giaTri = Kteam1.GetValue(1);
+
+
+            for (int i = 0; i < Kteam1.Length; i++) // Vì các phần tử có chỉ số là 0 1 2 nên điều kiện dừng là i < 3
             {
-                for (int j = 0; j < 10; j++)
-                {
-                    Console.WriteLine("biến J là: " + j);
-                }
-                Console.WriteLine("biến I là: "  + i);
+                Console.WriteLine("đang ở phần tử: " + Kteam1[i]);
+            }
+
+            Console.WriteLine(giaTri);
+
+            List<String> a = new List<string>();
+            a.Add("a");
+            a.Add("d");
+            a.Add("l");
+            a.Insert(10, "v");
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
             }
             Console.ReadKey();
         }
